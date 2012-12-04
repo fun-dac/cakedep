596c2f48c35f854964b1d623e5cdf290101a15f3
dex1t <exitest08@gmail.com>
2012-12-05 05:19:01 +0900

    .gitkeepの権限を変更しないように

--- hook/hook.sh
+++ hook/hook.sh
@@ -13,5 +13,11 @@ cd $1 #CakePHPルートへ移動
 #環境依存ファイルの変更
 cp app/config/path.php.development app/config/path.php
 
-chmod -R 777 app/tmp/ #log,cacheのパーミッション書き換え
+#ディレクトリのみ権限変更
+chmod 777 app/tmp/logs
+chmod 777 app/tmp/cache
+chmod 777 app/tmp/cache/models
+chmod 777 app/tmp/cache/persistent
+chmod 777 app/tmp/cache/views
+
 ruby set_rebase.rb $1 $2 #RewiteBaseの書き換え
